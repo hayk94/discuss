d3f8fdb8e8fcee1e9a0465d55d7b0ca2457d8053
Hayk <hayk_safaryan@epam.com>
2019-09-01 00:51:51 +0400

    implement comments functionality with broadcast socket

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -57,8 +57,10 @@ const createSocket = (topicId) => {
   // Now that you are connected, you can join channels with a topic:
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { renderComments(resp.comments) })
     .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on(`comments:${topicId}:new`, renderComment)
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
@@ -67,5 +69,25 @@ const createSocket = (topicId) => {
 }
 
 
+function renderComments(comments) {
+  const renderedComments = comments.map((comment) => {
+    return commentTemplate(comment)
+  })
+
+  document.querySelector('.collection').innerHTML = renderedComments.join()
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment)
+  document.querySelector('.collection').innerHTML += renderedComment
+}
+
+function commentTemplate(comment) {
+  return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `
+}
 
 window.createSocket = createSocket
